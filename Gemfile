b13d08376c245ff289128db896aff65430837b55
Rhys Gittoes <rhysgittoes@Rhyss-MacBook-Air.local>
2018-02-19 10:51:14 +0800

    facebook login works

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'clearance'
+gem 'omniauth'
 gem 'omniauth-facebook'
 gem "figaro"
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, controller: "users", only: [:create] do
+  resources :users, controller: "users", only: [:create, :edit] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
   get "/sign_up" => "users#new", as: "sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'pages#index'
+
+get "/auth/:provider/callback" => "sessions#create_from_omniauth"
 end
+
+
 
